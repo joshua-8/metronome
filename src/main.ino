2c368f3ff344442966c29f336155ad7a6fc86205
joshua-8 <joshua262144@gmail.com>
2025-01-05 13:20:26 -0800

    add more frequencies

--- src/main.ino
+++ src/main.ino
@@ -19,7 +19,7 @@ const unsigned long lockTimeMillis = 4000; // this many milliseconds after a fre
 unsigned long blinkDurationMicros = 50000; // the led turns on for this many microseconds
 const unsigned long buttonTimeout1 = 50;
 const unsigned long buttonTimeout2 = 500;
-const unsigned long buttonTimeout3 = 75;
+const unsigned long buttonTimeout3 = 40;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -29,13 +29,14 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 CRGB led = CRGB(255, 0, 0);
 
-#define NUM_FREQS 39
+#define NUM_FREQS 221
 
-const int frequencies[39] = { 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144, 152, 160, 168, 176, 184, 192, 200, 208 };
+// const int frequencies[39] = { 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144, 152, 160, 168, 176, 184, 192, 200, 208 };
+const int frequencies[221] = { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250 };
 
-int frequency = 100;
+int frequency = 20;
 
-int setFrequencyIndex = 21;
+int setFrequencyIndex = 70;
 
 int savedFrequencyIndex = 0;
 
@@ -87,7 +88,7 @@ void setup()
     setFrequencyIndex = EEPROM.read(frequency_eeprom_address);
     savedFrequencyIndex = setFrequencyIndex;
 
-    display.setRotation(2);
+    display.setRotation(0);
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE); // Draw white text
     display.clearDisplay();
